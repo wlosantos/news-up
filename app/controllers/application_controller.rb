e938b24b6557b8a2fb2610e9bcfebe339b2b8730
Wendel Lopes <wendelopes@msn.com>
2021-03-03 22:54:26 -0300

    Setting user devise with authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+
+  before_action :authenticate_user!, except: [:index]
+  before_action :configure_permited_parameters, if: :devise_controller?
+
+  layout :layout_by_resource
+
+  private
+
+  def layout_by_resource
+    'authentication' if devise_controller?
+  end
+
+  def configure_permited_parameters
+    devise_parameter_sanitizer.permit :sign_up, keys: %i[ name username kind status ]
+    devise_parameter_sanitizer.permit :account_update, keys: %i[ name ]
+  end
+
 end
