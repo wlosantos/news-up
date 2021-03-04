eecf23994d894fda790dc6b68377c1bf8c45767c
Wendel Lopes <wendelopes@msn.com>
2021-03-04 00:34:59 -0300

    Configured i18n pt-br and espanhol

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
 
+  before_action :set_locale
   before_action :authenticate_user!, except: [:index]
   before_action :configure_permited_parameters, if: :devise_controller?
 
   layout :layout_by_resource
+
+  def set_locale
+    I18n.locale = 'pt-BR'
+  end
 
   private
 
