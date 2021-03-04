a59cd5aeba052bf950107998a6cfb294cfd0f597
Wendel Lopes <wendelopes@msn.com>
2021-03-03 23:27:35 -0300

    Styled page user authentication

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@ import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "packs/custom"
+import 'packs/flashMessages'
 
 Rails.start()
 ActiveStorage.start()
