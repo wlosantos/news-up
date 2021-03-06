d3068395f43f94b1c2c5e114f8fa3c5cdacf41d0
Wendel Lopes <wendelopes@msn.com>
2021-03-06 05:31:04 -0300

    Added i18n and comments

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,12 @@ Rails.application.routes.draw do
       put 'unlike', to: 'posts#downvote'
     end
   end
+  
   get '/list_posts', to: 'posts#list_posts'
   get '/list_videos', to: 'posts#list_videos'
+  get 'tags/:tag', to: 'posts#index', as: :tag
   # get '/articles', to: 'posts#index', param: :post
   # get '/videos', to: 'posts#index', param: :video
-  get 'tags/:tag', to: 'posts#index', as: :tag
 
   resources :comments, only: %i[ create updade destroy ] do
     member do
